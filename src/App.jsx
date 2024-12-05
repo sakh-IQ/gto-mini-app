e6a9cb64e8e20260bf60060090d02faec768a3a4
IQ <Sakh.iq@yandex.ru>
2024-12-05 20:32:39 +1100

    id

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useTelegramWebApp } from './hooks/useTelegramWebApp';
 
 // Добавляем константы для отправки данных
 const TELEGRAM_BOT_TOKEN = "8025818266:AAFcZeopGZsTIUWZNRml4rrllioR0pCRo88";
-const TELEGRAM_CHAT_ID = "4738151106";
+const TELEGRAM_CHAT_ID = "-4738151106";
 const EMAIL = "sakh.crazy.kill@gmail.com";
 
 const App = () => {
